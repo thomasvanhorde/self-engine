d7f526125a3074429aa6615597b3ba0818bf7902
listentruth@gmail.com <listentruth@gmail.com@080f1aed-a245-2b98-5560-05af2b1d86cf>
2011-04-14 19:33:27 +0000

    update media interface

--- controller/admin/media/media.controller.php
+++ controller/admin/media/media.controller.php
@@ -13,9 +13,12 @@ class media_controller {
     }
 
     function defaut(){
-        $youtube = Base::Load(CLASS_YOUTUBE);
-        $youtube->uploadForm();
         $this->_view->addBlock('content','defaut.tpl');
+    }
+
+    function getYoutubeForm(){
+        $redirect = 'http://'.HTTP_HOST.'/admin/media/upload-youtube/'.$_GET['nodeID'].'/';
+        exit(BASE::Load(CLASS_YOUTUBE)->uploadForm($redirect));
     }
 
     function data(){
@@ -47,9 +50,30 @@ class media_controller {
                 $upload->file_src_mime,
                 array('url' => $upload->file_dst_pathname)
             );
-            header('location: ../');
+            header('location: ../');exit();
         } else {
             echo 'error : ' . $upload->error;
         }
+    }
+
+    function uploadYoutbe(){
+        $video = BASE::Load(CLASS_YOUTUBE)->getVideoEntry($_GET['id']);
+        $video->setVideoPrivate();
+        $data['id'] = $_GET['id'];
+        $data['pictures'] = $video->getVideoThumbnails();
+        $data['embed'] = 'http://www.youtube.com/embed/'.$_GET['id'];
+        $data['flashPlayer'] = $video->getFlashPlayerUrl();
+        $data['duration'] = $video->getVideoDuration();
+        $data['title'] = $video->getVideoTitle();
+        $data['description'] = $video->getVideoDescription();
+        
+        $this->_classMedia->addNode(
+            $_GET['param'][0],
+            $data['title'],
+            'videoYoutube',
+            array('dataYoutube' => $data)
+        );
+
+        header('location: ../../');exit();
     }
 }
