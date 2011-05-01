617e59c276cde5289d483d59c60b86c17611652c
listentruth@gmail.com <listentruth@gmail.com@080f1aed-a245-2b98-5560-05af2b1d86cf>
2011-05-01 14:45:33 +0000

    class extensions

--- controller/encode/encode.controller.php
+++ controller/encode/encode.controller.php
@@ -3,7 +3,12 @@
 class encode_controller extends Component{
 
     public  function defaut(){
-       echo selEncode($_GET['param'][0], ENCODE_METHOD);
+       if(isset($_GET['param'][1]))
+            $encode = $_GET['param'][1];
+       else
+            $encode = ENCODE_METHOD;
+        
+       echo selEncode($_GET['param'][0], $encode);
     }
 
 }
