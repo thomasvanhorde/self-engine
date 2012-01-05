f8708ee04eb875804dd473174e5f0cba29d662f5
aenyhm@gmail.com <aenyhm@gmail.com@080f1aed-a245-2b98-5560-05af2b1d86cf>
2012-01-05 18:55:10 +0000

    Added some notices and a reporting on used classes.

--- inc/class.config.php
+++ inc/class.config.php
@@ -15,13 +15,11 @@ define('CLASS_CONTENT_MANAGER', 'ContentManager');
 define('CLASS_SIMPLE_CM',       'SimpleContentManager');
 define('CLASS_PDF',             'Pdf');
 define('CLASS_LESS',            'Less_php');
-define('CLASS_TURBINE',         'Turbine');
 define('CLASS_MEDIA',           'Media');
 define('CLASS_YOUTUBE',         'Youtube');
 define('CLASS_UPLOAD',          'Upload');
 define('CLASS_WATERMARK',       'Watermark');
 define('CLASS_CALENDAR',        'Calendar');
-define('CLASS_IP_LOCATOR',      'Ip_locator');
 define('CLASS_MEMCACHE',        'MemcacheInstance');
 define('CLASS_QR',              'QR');
 
@@ -31,16 +29,19 @@ define('CLASS_QR',              'QR');
   Replace this by an autoload stack will increase performances and remove those
   useless declarations, don't you think? Like below.
 
-  // PHP5.3+ way:
+  PHP5.3+ way:
 
   spl_autoload_register(function ($className) {
     require_once sprintf('class/%s.php', $className);
   });
 
-  // Or for older versions if you want to keep the compatibility (but keep in
-  // mind that PHP5.3 is 2x much faster than PHP5.2 ;-)):
+  Or for older versions if you want to keep the compatibility (but keep in mind
+  that PHP5.3 is 2x much faster than PHP5.2 ;-)):
 
   function __autoload($className) {
     require_once sprintf('class/%s.php', $className);
   }
+
+  Or combine it with the ClassLoader file, but I do not think that we need to
+  define all these prefixes.
 ----------------------------------------------------------------------------- */
